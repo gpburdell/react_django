e21f7bb22311a2c82ade9ee7d8a8a08ca0820e2e
RichLindenberg <rlindenberg@wje.com>
2022-02-10 09:20:45 -0500

    updates plotly plot3 POST

--- frontend/src/components/Plotv3.js
+++ frontend/src/components/Plotv3.js
@@ -15,15 +15,39 @@ class Graph3  extends React.Component {
       // console.log(this.props.gage)
 
       //URL URL
-      let url = 'http://localhost:8000/api/nyc/gage/'+this.props.gage
+      // let url = 'http://localhost:8000/api/nyc/gage/'+this.props.gage
+      let url = 'http://localhost:8000/api/nyc/gage/'
       console.log(this.state.gage)
-      const res = await fetch(url);
-      const info = await res.json();
+      let date="2022-01-01 00:00:00"
+      // const res = await fetch(url);
+      // 
+      const response = await fetch(url,{
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({'gage':this.props.gage, 'from_date':date})
+        })
+
+      const info = await response.json();
       console.log(info)
       this.setState({
         data: info.data,
         layout: info.layout
       });
+
+    //   const addFeedback = async (newFeedback) => {
+    //     const response = await fetch('/feedback',{
+    //         method:'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify(newFeedback)
+    //     })
+    //     const data = await response.json()
+
+    //     setFeedback([data, ...feedback])
+    // }
       
   }
 
--- frontend/src/screens/PlotScreen3.js
+++ frontend/src/screens/PlotScreen3.js
@@ -1,15 +1,16 @@
 import React,{useState, useEffect} from 'react'
 import Graph3 from '../components/Plotv3'
 import { useParams } from 'react-router-dom'
-
+import Loader from '../components/Loader'
 
 function PlotScreen3() {
   const {gage} = useParams();
   return (
     
     <div>
-      <h1>Latest products</h1>
+      <Loader />
       <Graph3 gage={gage}/>
+      <Graph3 gage={'temp_40_avg'}/>
 
     </div>
   )
